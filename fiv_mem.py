93bbd3720f0c84c1c0f9b9fc292e366149b59f74
Ryushi-tech <mryrys@gmail.com>
2019-09-30 22:54:39 +0900

    fib_mem update2

--- fiv_mem.py
+++ fiv_mem.py
@@ -1,13 +1,23 @@
-m = 20
+import cProfile
+m = 300
 
 def fib(n):
     mem = [0]*(n+1)
-    if n <= 1:
-        return n
-    else:
+    def _fib(n):
+        if n <= 1:
+            return n
         if mem[n] != 0:
             return mem[n]
-    mem[n] = fib(n-1) + fib(n-2)
-    return mem[n]
+        mem[n] = _fib(n-1) + _fib(n-2)
+        return mem[n]
+    return _fib(n)
+
+def _fib_(n):
+    if n <= 1:
+        return n
+    return _fib_(n-1) + _fib_(n-2)
 
 print(fib(m))
+#print(fib(m), _fib_(m))
+cProfile.run("fib(m)")
+#cProfile.run("_fib_(m)")
