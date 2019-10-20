24fc305f535698979de577cc2864c121bae40ab9
Ryushi-tech <mryrys@gmail.com>
2019-10-20 20:28:11 +0900

    ABC 143 updates2

--- ABC_143D.py
+++ ABC_143D.py
@@ -2,16 +2,12 @@ import bisect
 
 n = int(input())
 lis = list(map(int,input().split()))
+lis.sort()
 
-a = sorted(lis, reverse=True)
-c = sorted(lis)
 d = 0
 
-for i in range(n):
-    for j in range(i + 1, n):
-        e = c[:-j-1]
-        b = bisect.bisect_right(e, int(a[i]-a[j]))
-        d += max(0, len(e) - b)
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        b = bisect.bisect_left(lis, int(lis[i] + lis[j]))
+        d += b - j - 1
 print(d)
-
-
