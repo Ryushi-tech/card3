607c4fa13a906b6287681e2bd1e9ff4217cc26f0
Ryushi-tech <mryrys@gmail.com>
2019-10-27 22:58:24 +0900

    darker 33A updates2

--- AGC_33A.py
+++ AGC_33A.py
@@ -17,7 +17,6 @@ while st < len(Q):
     for i in [[-1,0], [1,0], [0,-1], [0,1]]:
         x = Q[st][0] + i[0]
         y = Q[st][1] + i[1]
-        print(x,y)
         if 0 <= x < h and 0 <= y < w and V[x][y] == 0:
             V[x][y] = 1
             Q.append([x,y,Q[st][2]+1])
