3fe8b383a48c678dda9e51a89a2d224d682a9e00
Ryushi-tech <mryrys@gmail.com>
2019-10-29 22:24:10 +0900

    ABC 144 updates2

---
+++ ABC_144D.py
@@ -0,0 +1,14 @@
+import math
+a, b, x = map(int, input().split())
+h = x/a**2
+
+if b == h:
+    print(0)
+elif h >= b/2:
+    tanth = a / (b - h) / 2
+    theta = math.degrees(math.atan(tanth))
+    print(90 - theta)
+else:
+    tanth = 2 * a * h / b**2
+    theta = math.degrees(math.atan(tanth))
+    print(90 - theta)
