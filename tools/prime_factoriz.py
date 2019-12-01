75cf3df051f0c5a5ac89f271ab29e39784b0b8b1
Ryushi-tech <mryrys@gmail.com>
2019-12-01 11:52:18 +0900

    prime factoriz updates

--- tools/prime_factoriz.py
+++ tools/prime_factoriz.py
@@ -1,9 +1,8 @@
 pf = {}
 m = 1999998
-for i in range(2, int(m**0.5)+1):
-    while m % i == 0:
-        print(m)
-        pf[i] = pf.get(i, 0)+1
+for i in range(2, int(m**0.5) + 1):
+    while not m % i:
+        pf[i] = pf.get(i, 0) + 1
         m //= i
 if m > 1:
     pf[m] = 1
