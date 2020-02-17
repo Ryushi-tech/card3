97017323522ff0d1d1975b383fe8ea4376c7a17b
Ryushi-tech <mryrys@gmail.com>
2020-02-18 08:42:29 +0900

    Bricks updates

--- ABC155_C_2.py
+++ ABC155_C_2.py
@@ -7,7 +7,8 @@ a = [input().rstrip() for _ in range(q)]
 
 dic = collections.Counter(a)
 
-keys = [k for k, v in dic.items() if v == max(dic.values())]
+ma = max(dic.values())
+keys = [k for k, v in dic.items() if v == ma]
 keys_s = sorted(keys)
 for i in keys_s:
     print(i)
---
+++ tools/bricks_game.py
@@ -0,0 +1,18 @@
+t = int(input())
+for t_itr in range(t):
+    arr_count = int(input())
+    arr = list(map(int, input().rstrip().split()))
+    lenarr = len(arr)
+    dp = [0] * (lenarr + 1)
+    bricks = list(reversed(arr))
+    suma = [0] * (lenarr + 1)
+    for i in range(1, len(arr) + 1):
+        suma[i] = suma[i - 1] + bricks[i - 1]
+        if i <= 3:
+            dp[i] = sum(bricks[:i])
+        else:
+            a = bricks[i - 1] - dp[i - 1] + suma[i - 1]
+            b = sum(bricks[i - 2:i]) - dp[i - 2] + suma[i - 2]
+            c = sum(bricks[i - 3:i]) - dp[i - 3] + suma[i - 3]
+            dp[i] = max(a, b, c)
+    print(dp[lenarr])
