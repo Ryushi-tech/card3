19ace11676a1727e48378b37d7a04837c6bea56f
Ryushi-tech <52338292+Ryushi-tech@users.noreply.github.com>
2020-02-24 12:32:11 +0900

    Update ABC_141D.py

--- ABC_141D.py
+++ ABC_141D.py
@@ -1,13 +1,13 @@
 import heapq
 
-n,m=[10,1]
-a=[100,100,100,100,100,100,100,100,100,100]
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
 b=[-int(i) for i in a]
 
 heapq.heapify(b)
 
 for i in range(m):
-    x=heapq.heappop(b)//2
-    heapq.heappush(b,x)
+    x=-heapq.heappop(b)//2
+    heapq.heappush(b,-x)
 
 print(-sum(b))
