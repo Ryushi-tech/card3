5eaa9c540794d1ef39796b8620a4b9435b373a6f
Ryushi-tech <mryrys@gmail.com>
2020-07-17 20:56:20 +0900

    Add Shopping Street

--- void/solve.py
+++ void/solve.py
@@ -2,37 +2,28 @@ from card3.void.stop_watch import stop_watch
 
 @stop_watch
 def solve():
-    import heapq
+    import numpy as np
     n = int(input())
-    g = [[] for _ in range(n)]
-    for _ in range(n - 1):
-        a, b, c = map(int, input().split())
-        a, b = a - 1, b - 1
-        g[a].append((b, c))
-        g[b].append((a, c))
-    inf = 10**14
-    dist = [inf] * n
+    oc = [list(map(int, input().split())) for _ in range(n)]
+    prof = [list(map(int, input().split())) for _ in range(n)]
 
-    def dijkstra(s):
-        q = []
-        dist[s] = 0
-        heapq.heappush(q, [0, s])
-        while q:
-            p, v = heapq.heappop(q)
-            if dist[v] < p:
-                continue
-            for i, x in g[v]:
-                if dist[i] > dist[v] + x:
-                    dist[i] = dist[v] + x
-                    heapq.heappush(q, [dist[i], i])
+    oc = np.array(oc)
+    prof = np.array(prof)
 
-    m, k = map(int, input().split())
-    k = k - 1
-    dijkstra(k)
-    for _ in range(m):
-        e, f = map(int, input().split())
-        res = dist[e - 1] + dist[f - 1]
-        print(res)
+    inf = 10 ** 10
+    ans = - inf
+
+    for a in range(1, 1 << 10):
+        res = np.zeros(n, dtype=np.int64)
+        for b in range(10):
+            if a >> b & 1:
+                res += oc[:, b]
+        tmp = 0
+        for i, x in enumerate(res):
+            tmp += prof[i, x]
+        ans = max(ans, tmp)
+    print(ans)
 
 
 solve()
+
