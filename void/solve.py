5f5d01c521486d9fc4c78ff29b0783e8717769c6
Ryushi-tech <mryrys@gmail.com>
2020-09-11 14:41:05 +0900

    Update SegTree

--- void/solve.py
+++ void/solve.py
@@ -1,40 +1,69 @@
-mod = (10 ** 9) + 7
-lim = 1100000
-
-fct = [0] * lim
-inv_f = [0] * lim
-
-fct[0] = 1
-for i in range(1, lim):
-    fct[i] = fct[i - 1] * i % mod
-
-inv_f[lim - 1] = pow(fct[lim - 1], mod - 2, mod)
-
-for i in range(lim - 1, 0, -1):
-    inv_f[i - 1] = inv_f[i] * i % mod
+import sys
+input = lambda: sys.stdin.readline()
 
 
-def nCk(a, b):
-    if a < b:
-        return 0
-    else:
-        return fct[a] * inv_f[b] * inv_f[a - b] % mod
+def solve():
+    def init(init_arr):
+        global num, segfunc, ide_ele, seg
+        segfunc = lambda x, y: max(x, y)
+        ide_ele = 0
+        num = 2 ** (n - 1).bit_length()
+        seg = [ide_ele] * 2 * num
+        for i in range(n):
+            seg[i + num - 1] = init_arr[i]
+        for i in range(num - 2, -1, -1):
+            seg[i] = segfunc(seg[2 * i + 1], seg[2 * i + 2])
+
+    def update(k, x):
+        k += num - 1
+        seg[k] = x
+        while k:
+            k = (k - 1) // 2
+            seg[k] = segfunc(seg[k * 2 + 1], seg[k * 2 + 2])
+
+    def query(p, q):
+        if q <= p:
+            return ide_ele
+        p += num - 1
+        q += num - 2
+        res = ide_ele
+        while q - p > 1:
+            if p & 1 == 0:
+                res = segfunc(res, seg[p])
+            if q & 1 == 1:
+                res = segfunc(res, seg[q])
+                q -= 1
+            p = p // 2
+            q = (q - 1) // 2
+        if p == q:
+            res = segfunc(res, seg[p])
+        else:
+            res = segfunc(segfunc(res, seg[p]), seg[q])
+        return res
+
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    init(a)
+    res = []
+
+    for _ in range(q):
+        t, a, b = map(int, input().split())
+        if t == 1:
+            update(a - 1, b)
+        elif t == 2:
+            res.append(query(a - 1, b))
+        elif t == 3:
+            ng = a - 1
+            ok = n + 1
+            while ok > ng + 1:
+                mid = (ok + ng) // 2
+                if query(ng, mid) >= b:
+                    ok = mid
+                else:
+                    ng = mid
+            res.append(ok)
+    print("\n".join(map(str, res)))
 
 
-n, k = map(int, input().split())
-s = list(map(int, input().split()))
-s.sort()
-
-s_max = 0
-s_min = 0
-
-for i in range(k - 1, n):
-    s_max = s_max + s[i] * nCk(i, k - 1)
-    s_max %= mod
-
-for i in range(n - k + 1):
-    s_min = s_min + s[i] * nCk(n - i - 1, k - 1)
-    s_min %= mod
-
-print((s_max-s_min) % mod)
-# print(s_max, s_min)
+solve()
